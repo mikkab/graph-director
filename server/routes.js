a77f9a8a2f62fe5f1a8d32132cb0fa0b53139b26
asafdav2 <asafdav2@gmail.com>
2014-09-01 16:38:51 +0300

    use caching

--- server/routes.js
+++ server/routes.js
@@ -6,8 +6,11 @@
 
 var errors = require('./components/errors');
 var MongoClient = require('mongodb').MongoClient
+var NodeCache = require('node-cache');
 var _ = require('lodash');
 var db;
+
+var cache = new NodeCache();
 
 
 // open a connection and memoize it
@@ -27,41 +30,74 @@ function connect(callback) {
 module.exports = function(app) {
 
   app.route('/api/director/:name').get(function(req, res) {
-     connect(function(err, db) {
+      var name = req.params.name;
+      var key = name;
+      cache.get(key, function(err, value) {
         if (err) throw err;
 
-        db.collection('directors').find({name : req.params.name}).toArray(function(err, results) {
-          if (err) throw err;
-          res.end(JSON.stringify(results));
-        });
+        if (!_.isEmpty(value)) {
+          res.end(value[key]);
+        } else {
+          connect(function(err, db) {
+            if (err) throw err;
+            db.collection('directors').find({name : name}).toArray(function(err, results) {
+              if (err) throw err;
+              var json = JSON.stringify(results);
+              cache.set(key, json);
+              res.end(json);
+            });
+          });
+        }
+      });
     });
-  });
 
   app.route('/api/directors').get(function(req, res) {
-     connect(function(err, db) {
-        if (err) throw err;
-        var filter = req.query.filter || '';
-        if (filter.length < 2)
-          filter = '';
+    var key = '_directors_';
+    cache.get(key, function(err, value) {
+      if (err) throw err;
 
-        db.collection('directors').find({name: {$regex : filter}}, {_id : 0, name : 1}).sort({name : 1}).toArray(function(err, results) {
+      if (!_.isEmpty(value)) {
+        res.end(value[key]);
+      } else {
+        connect(function(err, db) {
           if (err) throw err;
-          var names = _.pluck(results, 'name');
-          res.end(JSON.stringify(names));
+          var filter = req.query.filter || '';
+          if (filter.length < 2)
+            filter = '';
+
+          db.collection('directors').find({name: {$regex : filter}}, {_id : 0, name : 1}).sort({name : 1}).toArray(function(err, results) {
+            if (err) throw err;
+            var names = _.pluck(results, 'name');
+            var json = JSON.stringify(names);
+            cache.set(key, json);
+            res.end(json);
+          });
         });
+      }
     });
   });
 
   app.route('/api/directors/top').get(function(req, res) {
-     connect(function(err, db) {
-        if (err) throw err;
+    var key = '_top_';
+    cache.get(key, function(err, value) {
+      if (err) throw err;
 
-        var limit = req.params.limit ? parseInt(req.params.limit) : 200;
-        db.collection('directors').find({}, {_id : 0, name : 1, average : 1}, {limit: limit}).sort({average : -1}).toArray(function(err, results) {
+      if (!_.isEmpty(value)) {
+        res.end(value[key]);
+      } else {
+        connect(function(err, db) {
           if (err) throw err;
-          var names = _.pluck(results, 'name');
-          res.end(JSON.stringify(names));
+
+          var limit = req.params.limit ? parseInt(req.params.limit) : 200;
+          db.collection('directors').find({}, {_id : 0, name : 1, average : 1}, {limit: limit}).sort({average : -1}).toArray(function(err, results) {
+            if (err) throw err;
+            var names = _.pluck(results, 'name');
+            var json = JSON.stringify(names);
+            cache.set(key, json);
+            res.end(json);
+          });
         });
+      }
     });
   });
   
