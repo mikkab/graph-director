685b734510672b7e82ab76ae92e86b7cb97e87cf
asafdav2 <asafdav2@gmail.com>
2014-09-03 07:10:35 +0300

    remove mongoose

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -3,10 +3,4 @@
 // Development specific configuration
 // ==================================
 module.exports = {
-  // MongoDB connection options
-  mongo: {
-    uri: 'mongodb://localhost/graphdirector-dev'
-  },
-
-  seedDB: true
 };
