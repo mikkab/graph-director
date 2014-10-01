2492a47ef2d0901ff0be096b8e0b2e0d61bf9ee5
asafdav2 <asafdav2@gmail.com>
2014-10-01 22:18:46 +0300

    clean server controller, routes.js

--- server/api/director/director.controller.js
+++ server/api/director/director.controller.js
@@ -2,7 +2,11 @@
 
 var _ = require('lodash');
 var MongoClient = require('mongodb').MongoClient;
+var NodeCache = require('node-cache');
+var _ = require('lodash');
+var db;
 
+var cache = new NodeCache();
 
 // Get a single director
 exports.show = function(req, res) {
@@ -21,23 +25,107 @@ exports.show = function(req, res) {
 };
 
 // Get director names
-exports.list = function(req, res) {
-  MongoClient.connect('mongodb://127.0.0.1:27017/graph_directors', function(err, db) {
-      if (err) { return handleError(res, err); }
+exports.get_director = function(req, res) {
+    var name = req.params.name;
+    var key = name;
+    cache.get(key, function(err, value) {
+      if (err) throw err;
 
-      var directors = db.collection('directors');
-      var filter = req.params.filter; 
-      collection.find( {name: '/.*' + filter + '/.*' }, {_id: 0, name: 1}, function(err, directors) {
-
-        if (err) { return handleError(res, err); }
-        return res.json(_.pluck(directors, 'name'));
-      });
-  });
+      if (!_.isEmpty(value)) {
+        res.end(value[key]);
+      } else {
+        connect(function(err, db) {
+          if (err) throw err;
+          db.collection('directors').find({name : name}).toArray(function(err, results) {
+            if (err) throw err;
+            var json = JSON.stringify(results);
+            cache.set(key, json);
+            res.end(json);
+          });
+        });
+      }
+    });
 };
+
+exports.invalidate_director = function(req, res) {
+  var name = req.params.name;
+  var key = name;
+  res.end(cache.del(key));
+}
+
+exports.get_directors = function(req, res) {
+  var key = '_directors_';
+  cache.get(key, function(err, value) {
+    if (err) throw err;
+
+    if (!_.isEmpty(value)) {
+      res.end(value[key]);
+    } else {
+      connect(function(err, db) {
+        if (err) throw err;
+        var filter = req.query.filter || '';
+        if (filter.length < 2)
+          filter = '';
+
+        db.collection('directors').find({name: {$regex : filter}}, {_id : 0, name : 1}).sort({name : 1}).toArray(function(err, results) {
+          if (err) throw err;
+          var names = _.pluck(results, 'name');
+          var json = JSON.stringify(names);
+          cache.set(key, json);
+          res.end(json);
+        });
+      });
+    }
+  });
+}
+
+exports.invalidate_directors = function(req, res) {
+  var key = '_directors_';
+  res.end(cache.del(key));
+}
+
+exports.get_top_directors = function(req, res) {
+  var key = '_top_';
+  cache.get(key, function(err, value) {
+    if (err) throw err;
+
+    if (!_.isEmpty(value)) {
+      res.end(value[key]);
+    } else {
+      connect(function(err, db) {
+        if (err) throw err;
+
+        var limit = req.params.limit ? parseInt(req.params.limit) : 200;
+        db.collection('directors').find({}, {_id : 0, name : 1, average : 1}, {limit: limit}).sort({average : -1}).toArray(function(err, results) {
+          if (err) throw err;
+          var names = _.pluck(results, 'name');
+          var json = JSON.stringify(names);
+          cache.set(key, json);
+          res.end(json);
+        });
+      });
+    }
+  });
+}
+
+exports.invalidate_top_directors = function(req, res) {
+  var key = '_top_';
+  res.end(cache.del(key));
+}
 
 function handleError(res, err) {
   return res.send(500, err);
 }
 
-function connect() {
+// open a connection and memoize it
+function connect(callback) {
+  if (db === undefined) {
+    MongoClient.connect(process.env.MONGOHQ_URL, function(err, _db) {
+        if (err) { return callback(err); }
+        db = _db;
+        callback(null, db);
+    });
+  } else {
+    callback(null, db);
+  }
 }
--- server/api/director/index.js
+++ server/api/director/index.js
@@ -5,6 +5,11 @@ var controller = require('./director.controller');
 
 var router = express.Router();
 
-router.get('/:name', controller.show);
+router.get('/director/:name', controller.get_director);
+router.delete('/director/:name', controller.invalidate_director);
+router.get('/directors', controller.get_directors);
+router.delete('/directors', controller.invalidate_directors);
+router.get('/directors/top', controller.get_top_directors);
+router.delete('/directors/top', controller.invalidate_top_directors);
 
 module.exports = router;
