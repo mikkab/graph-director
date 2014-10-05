f3011c7a4ba15a7f0f936f56c53936cae58a6a24
asafdav2 <asafdav2@gmail.com>
2014-10-05 10:42:10 +0300

    remove unneeded code

--- server/api/director/director.controller.js
+++ server/api/director/director.controller.js
@@ -8,22 +8,6 @@ var db;
 
 var cache = new NodeCache();
 
-// Get a single director
-exports.show = function(req, res) {
-  console.log('in show');
-  MongoClient.connect('mongodb://127.0.0.1:27017/graph_directors', function(err, db) {
-      if (err) { return handleError(res, err); }
-
-      var directors = db.collection('directors');
-      collection.find( {'name' : req.params.name}, function(err, director) {
-
-        if (err) { return handleError(res, err); }
-        if (!director) { return res.send(404); }
-        return res.json(director);
-      });
-  });
-};
-
 // Get director names
 exports.get_director = function(req, res) {
     var name = req.params.name;
